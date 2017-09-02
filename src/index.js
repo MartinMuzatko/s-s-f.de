4d824f5712f4cbc61fe35bb5308b95acf7321f54
Martin Muzatko <martin@happy-css.com>
2017-09-02 09:29:34 +0200

    Clean up unneeded code, remove unused components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import regeneratorRuntime from "regenerator-runtime"
-import riot from 'riot'
 import './less/main.less'
 import './flex.scss'
 import './components/user/user-register.html'
@@ -7,13 +5,5 @@ import './components/user/user-profile.html'
 import './components/event/event-create.html'
 // document.body.innerHTML = '<app></app>'
 riot.mount('*')
-/*
-var x = async function() {
-	var response = await fetch('/')
-	var data = await response.text();
-	console.log(data)
-}
-x()
-*/
 
 import './images/logo.png'
--- user-list.php
+++
@@ -1,8 +0,0 @@
-<?php namespace ProcessWire;?>
-<?$profiles = $users->find("*")?>
-<? foreach ($profiles as $profile): ?>
-    <? if ($profile->avatar->count): ?>
-        <img src="<?=$profile->avatar->first->size(32,32)->url?>" alt="<?=$profile->name?>">
-    <? endif ?>
-    <a href="<?=$this->config->urls->root?>user/<?=$profile->name?>"><?=$profile->name?></a> here since <?=$profile->created?> <br>
-<? endforeach ?>
