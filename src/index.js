fe60f01599e1b2b6390d05dbd04b9a5f29659782
Martin Muzatko <martin@happy-css.com>
2017-09-07 08:58:56 +0200

    Move src/components to src/js/components with updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './less/main.less'
-import './flex.scss'
-import './components/user/user-register.html'
-import './components/user/user-profile.html'
-import './components/event/event-create.html'
-// document.body.innerHTML = '<app></app>'
-riot.mount('*')
-
-import './images/logo.png'
---
+++ src/main.js
@@ -0,0 +1,22 @@
+import 'riot'
+import './less/main.less'
+import './flex.scss'
+import './js/components/event/edit/event-create.html'
+import './js/components/user/user-register.html'
+import './js/components/user/user-profile-dropdown.html'
+import './js/components/user/user-profile.html'
+import './js/components/user/user-profile-list.html'
+import './js/components/event/event-list-short.html'
+import './js/components/user/user-club-list.html'
+import './js/components/user/user-filter.html'
+import './js/components/user/user-message-send.html'
+
+window.riot = riot
+window.api = window.api || {}
+window.api.google = {}
+riot.observable(window.api.google)
+
+console.log((window.api.google));
+riot.mount('*')
+
+import './images/logo.png'
