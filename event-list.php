2695b6a5d353c4fcf730257b9493cd193e36dcee
Martin Muzatko <martin@happy-css.com>
2017-09-25 09:21:25 +0200

    Added event list

--- event-list.php
+++ event-list.php
@@ -1,13 +1,67 @@
 <?php namespace ProcessWire; ?>
-<? foreach ($page->children as $event): ?>
-    <h2>
-        <a href="<?=$event->url?>">
-            <?=$event->title?>
-        </a>
-    </h2>
-    <p>
-        <?=$event->ticketLimit - count($event->get('template=event-registrations')->children)?>/<?=$event->ticketLimit?>
-        Tickets noch frei
-    </p>
-    <hr>
-<? endforeach; ?>
+<?if($user->hasPermission('event-manage')):?>
+	<a href="<?=$pages->get('/events/create')->url?>" class="button button--primary">Neues Event erstellen</a>
+<?endif?>
+
+<div>
+	Neue Events
+	Events in Planung
+	Vergangene Events
+</div>
+
+<div class="filter-actions">
+	<a class="button button--primary" href="<?=$page->url?>?sort=-startDate">Neueste zuerst</a>
+	<a class="button button--primary" href="<?=$page->url?>?sort=startDate">Älteste zuerst</a>
+	<a class="button button--primary" href="<?=$page->url?>?filter=open">Nur offene Events</a>
+	<a class="button button--primary" href="<?=$page->url?>?filter=all">Alle Events</a>
+
+<div class="event-list">
+	<?
+		$sort = $input->get->sort ? trim($sanitizer->name($input->get->sort), ', ') : 'startDate';
+		$input->whitelist('sort', $sort);
+		$filter = $input->get->filter ? trim($sanitizer->name($input->get->filter), ', ') : 'all';
+		$input->whitelist('filter', $filter);
+		switch ($filter) {
+			case 'open':
+				$eventPages = $events->getOpenEvents();
+				break;
+			case 'all':
+			default:
+				$eventPages = $events->getEvents();
+				break;
+		}
+	?>
+	<? foreach ($eventPages->sort($sort) as $event): ?>
+		<?
+			$event->guestlist = $event->getPageByModule('event-guestlist');
+			$event->registration = $event->getPageByModule('event-registration');
+		?>
+		<article class="event-list__event event" layout="row">
+			<div class="event__column event__logo" flex="15" layout="row" layout-align="center center">
+				<a href="<?=$event->url?>">
+					<img src="<?=$event->logo ? $event->logo->url : ''?>" alt="<?=$event->title?>">
+				</a>
+			</div>
+			<div class="event__column event__name" flex="45">
+				<h2 layout="row">
+					<a href="<?=$event->url?>">
+						<?=$event->title?>
+					</a>
+				</h2>
+				<p>
+					<div>
+						<div>
+							Start: <?=$event->startDate?>
+						</div>
+						<div>
+							Ende: <?=$event->endDate?>
+						</div>
+					</div>
+				</p>
+			</div>
+			<div class="event__column" flex="40">
+				<? require('./partials/event/registerstate.php') ?>
+			</div>
+		</article>
+	<? endforeach; ?>
+</div>
