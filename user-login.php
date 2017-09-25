c589d3389d80a5a000664c97b0eeb9d132e9f65f
Martin Muzatko <martin@happy-css.com>
2017-09-25 09:43:03 +0200

    Redesign user login

--- user-login.php
+++ user-login.php
@@ -1,28 +1,51 @@
-<?php namespace ProcessWire; ?>
-<form action="" method="post">
-    <input type="text" name="username" value="">
-    <input type="password" name="password" value="">
-    <input type="submit" value="Login">
-</form>
-
-<? if($user->isLoggedin()): ?>
-    <a href="<?=$pages->get('/user/logout')->url?>">Logout <?=$user->name?></a>
-<? else: ?>
-    <a href="<?=$pages->get('/user/login')->url?>">Login</a>
-<? endif; ?>
-<?php
-?>
-<? if (count($input->post()->getArray())): ?>
-    <?php
-        $loginUser = $session->login($input->post->username, $input->post->password);
-        if (!$loginUser instanceof User) {
-            $loginUser = $session->login($input->post->username, $input->post->password);
+<?php namespace ProcessWire;
+    if (count($input->post()->getArray())) {
+        if (strstr($input->post->username, '@')) {
+            $username = $users->get("email=".$input->post->username);
+        } else {
+            $username = $input->post->username;
         }
-
-
-    ?>
-    <? if($loginUser instanceof User): ?>
-        logged in! Hello <?=$loginUser->name?>
-    <? endif; ?>
-    <? var_dump($loginUser); ?>
-<? endif; ?>
+        $loginUser = $session->login($username, $input->post->password);
+        if($loginUser instanceof User) {
+            $session->redirect($pages->get('/')->url.'users/'.$loginUser->name);
+        } else {
+            $error = 1;
+        }
+    }
+    if($user->isLoggedin()) {
+        $session->redirect($pages->get('/')->url.'users/'.$user->name);
+    }
+?>
+<form action="" method="POST" layout="column" layout-align="center center">
+    <div class="fieldset">
+        <p>
+            <a href="<?=$pages->get('/users/register')->url?>">Hast du noch keinen Account? Jetzt Registrieren.</a>
+        </p>
+        <user-login>
+            <div layout="column" layout-align="center center">
+                <img ref="avatar" class="avatar avatar--big avatar--round" src="<?=$user->getAvatar()?>" alt="">
+            </div>
+            <? if(isset($error)): ?>
+                <div class="notification notification--error">
+                    Der User existiert nicht oder das Passwort ist falsch.<br> Wenn du das Passwort vergessen hast, <a href="<?=$pages->get('/users/forgot-password')->url.'?name='?>">kannst du es wiederherstellen</a>.
+                </div>
+            <? endif ?>
+            <label class="field-group" for="username">
+                <span class="field-group__label">Nick / E-Mail</span>
+                <input ref="username" class="field-group__input" type="text" name="username" required autocomplete autofocus value="<?=$input->post->username?>">
+            </label>
+            <label class="field-group">
+                <span class="field-group__label">Passwort</span>
+                <input class="field-group__input" type="password" name="password" autocomplete required value="">
+            </label>
+            <input class="button button--primary button--block button--big" type="submit" value="Login">
+            <p>
+                <a href="<?=$pages->get('/users/forgot-password')->url?>">Passwort vergessen?</a>
+            </p>
+            <div>
+                <div class="text-or"><span>Oder</span></div>
+                <div class="g-signin2" data-onsuccess="googleSignIn"></div>
+            </div>
+        </user-login>
+    </div>
+</form>
