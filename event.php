186dc500a37d085569196dea09b3bbcd1fbb9e29
Martin Muzatko <martin@happy-css.com>
2017-09-26 10:46:05 +0200

    Remove event registration template and add views to event page

--- event.php
+++ event.php
@@ -1,22 +1,21 @@
-<?php namespace ProcessWire; ?>
-<p>Beginnt: <?=$page->startDate?></p>
-<p>Endet: <?=$page->endDate?></p>
-<?=$page->ticketLimit - count($page->get('template=event-registrations')->children)?>/<?=$page->ticketLimit?>
-tickets noch offen
-
-<p>
-    <a href="<?=$page->child('template=event-registrations')->url?>">Jetzt registrieren</a>
-</p>
-
-<?php
-    $event = new \API\Event($page);
-    var_dump($event->getTicketsLeft());
-
+<?php namespace ProcessWire;
+    $event = $page;
+    $event->guestlist = $event->getPageByModule('event-guestlist');
+    $event->registration = $event->getPageByModule('event-registration');
 ?>
-<? /*
+<?if($user->hasPermission('event-manage')):?>
+    <a href="<?=$page->url?>" class="button button--primary">Event Ansicht</a>
+    <a href="<?=$page->url?>edit" class="button button--primary">Event bearbeiten</a>
+    <a href="<?=$page->url?>manage" class="button button--primary">Event managen</a>
+	<a href="<?=$page->url?>manage-registrations" class="button button--primary">Registrierungen bearbeiten</a>
+<?endif?>
 
-isRegistrationOpen
-isRegistrationClosed
-getTicketsLeft
-getRegistrations
-*/
+<? if($input->urlSegment == 'edit'):?>
+    <event-update event-name="<?=$page->name?>"></event-update>
+<? elseif($input->urlSegment == 'manage'):?>
+    <? require('./partials/event/manage.php')?>
+<? elseif($input->urlSegment == 'manage-registrations'):?>
+    <? require('./partials/event/manage-registrations.php')?>
+<? else: ?>
+    <? require('./partials/event/view.php')?>
+<? endif; ?>
