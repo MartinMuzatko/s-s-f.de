316c3d5bda81a0286005cd3aaa9f6d042f2eada7
Martin Muzatko <martin@happy-css.com>
2017-09-28 15:45:04 +0200

    Added hardcoded demo text for home

--- home.php
+++ home.php
@@ -12,3 +12,21 @@
         </div>
     <? endforeach; ?>
 </ssf-carousel>
+<div class="content content--padded" layout="row" layout-align="space-between">
+	<div flex="45">
+		<h2 class="heading text-or"><span>Über uns</span></h2>
+		<p>Wir sind ein kleines Projekt welches den Furries im Süden Deutschlands die Möglichkeit bietet, ihre Veranstaltungen wie Meets, Cons und Walks zu koordinieren und zu präsentieren.</p>
+
+		<p>Möchtest Du ein Treffen organisieren und brauchst Hilfe? Dann greifen Wir Dir gerne unter die Arme! Du musst kein Vereinsmitglied sein um bei uns mitmachen zu können! Jeder ist herzlich dazu eingeladen unsere Plattform zu nutzen. Wir bieten Dir ein Eventmanagemant/Reg-System, ein Portal zum Helfer suchen und wir machen Dein Event auf den gängigen Social Media-Plattformen publik.</p>
+
+		<p>Dafür bieten wir Dir hier eine Infrastruktur an. Noch befindet sich einiges im Auf- sowie im Umbau. Wir sind fast täglich in unserer Freizeit dabei das Portal zu pflegen. </p>
+
+		<p>Wir freuen uns schon darauf Dir und Deinem Event bei Seite stehen zu können!</p>
+
+		<p>~Die Südstaaten Furs</p>
+	</div>
+	<div flex="45">
+		<h2 class="heading text-or"><span>Events</span></h2>
+		<event-list-short heading="Events die in Kürze starten"></event-list-short>
+	</div>
+</div>
