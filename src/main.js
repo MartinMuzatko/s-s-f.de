d2cedfe7254fbd6fa477e90083f7a344f0264c39
Martin Muzatko <martin@happy-css.com>
2017-09-28 15:48:30 +0200

    Added floating header using waypoints

--- src/main.js
+++ src/main.js
@@ -1,16 +1,56 @@
 import 'riot'
+import fittier from 'fitter-happier-text'
+import 'waypoints/lib/noframework.waypoints'
 import './less/main.less'
 import './flex.scss'
-import './js/components/event/edit/event-create.html'
+import './js/components/page/page-editor.html'
+import './js/components/ssf-carousel.html'
+import './js/components/ssf-countdown.html'
+import './js/components/ssf-chart.html'
+import './js/components/ssf-location-distance.html'
 import './js/components/user/user-register.html'
+import './js/components/user/user-login.html'
 import './js/components/user/user-profile-dropdown.html'
-import './js/components/user/user-profile.html'
+import './js/components/user/user-profile-card.html'
 import './js/components/user/user-profile-list.html'
 import './js/components/user/user-messages.html'
-import './js/components/event/event-list-short.html'
 import './js/components/user/user-club-list.html'
 import './js/components/user/user-filter.html'
 import './js/components/user/user-message-send.html'
+import './js/components/event/edit/event-update.html'
+import './js/components/event/edit/event-create.html'
+import './js/components/event/event-register.html'
+import './js/components/event/manage/manage-registrations.html'
+import './js/components/event/event-list-short.html'
+import './images/logo.png'
+import './images/navigation/events.svg'
+import './images/navigation/verein.svg'
+import './images/navigation/users.svg'
+import './images/navigation/home.svg'
+import './images/navigation/kontakt.svg'
+import './images/navigation/login.svg'
+
+fittier(document.querySelectorAll('.js-fitty'))
+
+function floatHeaderOnWaypoint () {
+    let offset = document.querySelector('.site__nav').offsetHeight
+    new Waypoint({
+        element: document.querySelector('.site__content'),
+        offset: offset,
+        handler: direction=>{
+            let offset = document.querySelector('.site__nav').offsetHeight
+            if (direction == 'down') {
+                if (document.body.clientWidth > 600) {
+                    document.querySelector('.site__nav').classList.add('site__nav--floating')
+                    document.querySelector('.site__content').style.marginTop = offset+16+'px'
+                }
+            } else {
+                document.querySelector('.site__nav').classList.remove('site__nav--floating')
+                document.querySelector('.site__content').style.marginTop = 0
+            }
+        }
+    })
+}
 
 window.riot = riot
 window.api = window.api || {}
@@ -20,4 +60,4 @@ riot.observable(window.api.google)
 riot.observable(window.api.user.track)
 riot.mount('*')
 
-import './images/logo.png'
+floatHeaderOnWaypoint()
