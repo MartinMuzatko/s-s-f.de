058a8863c6af9ab16e7ea106ace57ed10074fb1b
Martin Muzatko <martin@happy-css.com>
2018-04-15 21:58:36 +0200

    Made guestlist responsive

--- event-registration.php
+++ event-registration.php
@@ -1,6 +1,6 @@
 <?php namespace ProcessWire;?>
 <? if($options['mode'] == 'guestlist' && $page->profile): ?>
-    <user-profile-card flex="33"
+    <user-profile-card flex="100" flex-gt-sm="50" flex-gt-md="33" flex-gt-lg="25"
         joined="<?=date('Y-m-d', $page->profile->created)?>"
         link="<?=$this->config->urls->root?>users/<?=$page->profile->name?>"
         club-number="<?=$page->profile->clubMemberID?>"
